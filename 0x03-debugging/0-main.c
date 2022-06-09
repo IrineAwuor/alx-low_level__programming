97dffd75d7b70eb9ed0bae8a1e32bd552f0f1156
IrineAwuor <irineeliase@gmail.com>
2022-06-09 07:42:49 -0700

    cheking test file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,8 +4,7 @@
  * main - tests function that prints if integer is positive or negative
  *
  * Return: 0
- *
- * */
+ */
 
 int main(void)
 
@@ -14,7 +13,6 @@ int main(void)
 int i;
 
 i = 0;
-
 positive_or_negative(i);
 
 return (0);
