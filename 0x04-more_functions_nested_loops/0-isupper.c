635ad535036c7f7bc2cb9c3b4d77a959a1df7c3e
IrineAwuor <irineeliase@gmail.com>
2022-06-12 12:06:19 -0700

    a function that checks for uppercase character.

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio>
+
+/**
+ * int _isupper - funtion that checks for uppercase character.
+ *
+ * Return: 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		_puthar(alph);
+	}
+	return (0);
+}
+
