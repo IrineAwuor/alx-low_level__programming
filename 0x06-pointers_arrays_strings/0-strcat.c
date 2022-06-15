d780a8c1967c1b4825ff62987b2a54b2c7c40199
IrineAwuor <irineeliase@gmail.com>
2022-06-15 10:35:09 -0700

    a function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ *_strlen - Returns the length of a string
+ *@str: The string to get the length of.
+ *Return: The length of @str.
+ */
+
+int _strlen(char *str)
+{
+	int length = 0;
+
+	while (*str)
+		str++;
+		length++;
+
+	return (length);
+}
+
+/**
+ *_strcat - concatinates two strings
+ *@dest:destination pointer
+ *@src:pointer to a string
+ *Return: concatinated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *cat = dest + _strlen(dest);
+	int length =  _strlen(dest) + _strlen(src);
+
+	while (*src)
+	{
+		*cat += *src;
+		src++;
+		cat++;
+	}
+	*cat += '\0';
+	cat -= (length);
+	*dest = *cat;
+
+	return (cat);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h_included
+#define main_h_included
+
+char *_strcat(char *dest, char *src);
+
+#endif
