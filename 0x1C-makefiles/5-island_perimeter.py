540b67bee2635f658a49332da8f41907ee37c27f
IrineAwuor <irineeliase@gmail.com>
2022-11-16 23:15:17 -0800

    a function def island_perimeter(grid): that returns the perimeter of the island described in grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -17,4 +17,19 @@ def island_perimeter(grid):
                     p += 1  # left
                 if col == (len(grid[0]) - 1) or grid[row][col + 1] == 0:
                     p += 1  # right
-    return p 
+    return p
+
+
+# Method for rectangular island only, no odd shapes
+#    maxWidth = 0
+#    length = 0
+#    for i in range(len(grid)):
+#        width = 0
+#        for j in range(len(grid[0])):
+#            if grid[i][j] == 1:
+#                width += 1
+#        if width:
+#            length += 1
+#        if width > maxWidth:
+#            maxWidth = width
+#    return ((maxWidth + length) * 2)
